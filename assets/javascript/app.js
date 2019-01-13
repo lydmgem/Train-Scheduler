cf1bc43749ec97ddd4d15e5ac580b45737e02ae5
lydmgem <lydmgem@gmail.com>
2019-01-13 05:27:37 -0600

    Added some JS & HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,16 @@
-.container {
-    margin: 30px;
-}
+$("#submit-btn").on("click", function(event){
+    event.preventDefault();
+    
+    var name = $("#train-name").val().trim();
+    var destination = $("#destination").val().trim();
+    var firstTime = $("#first-train-time").val().trim();
+    var frequency = $("#frequency").val().trim();
+
+    console.log(name);
+    console.log(destination);
+    console.log(firstTime);
+    console.log(frequency);
+
+    // Append the information taken from the user input to 
+    $("#results-row").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + firstTime + "</td><td>" + frequency + "</td></tr>");
+});
