59f72c34eee884b64d4beea18abb82410f25b84e
lydmgem <lydmgem@gmail.com>
2019-01-16 18:35:44 -0600

    Commit 1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,18 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDPJW-7jdhConoe730Gl_UGJpLIYr15N5U",
+    authDomain: "train-scheduler-4922e.firebaseapp.com",
+    databaseURL: "https://train-scheduler-4922e.firebaseio.com",
+    projectId: "train-scheduler-4922e",
+    storageBucket: "",
+    messagingSenderId: "319232283463"
+  };
+  firebase.initializeApp(config);
+
+// Variable to reference database
+  var database = firebase.data();
+
+// On click function for the submit button to grab the user input
 $("#submit-btn").on("click", function(event){
     event.preventDefault();
     
@@ -11,6 +26,9 @@ $("#submit-btn").on("click", function(event){
     console.log(firstTime);
     console.log(frequency);
 
-    // Append the information taken from the user input to 
-    $("#results-row").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + firstTime + "</td><td>" + frequency + "</td></tr>");
+    // Append the information taken from the user input to the train schedule table
+    $("#results-body").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + firstTime + "</td><td>" + frequency + "</td></tr>");
+
+    // Appending the information from Firebase to the train schedule table
+
 });
