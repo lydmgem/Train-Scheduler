418c9e44b64d6e5490de4620fb2d4edf5c612885
lydmgem <lydmgem@gmail.com>
2019-01-19 16:42:41 -0600

    Checking functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,16 @@
 // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyDPJW-7jdhConoe730Gl_UGJpLIYr15N5U",
-    authDomain: "train-scheduler-4922e.firebaseapp.com",
-    databaseURL: "https://train-scheduler-4922e.firebaseio.com",
-    projectId: "train-scheduler-4922e",
-    storageBucket: "",
-    messagingSenderId: "319232283463"
+    apiKey: "AIzaSyD2h2mnT9QGBQHZmQc2fJMYIbMc9v1ZQVc",
+    authDomain: "class-example-fac60.firebaseapp.com",
+    databaseURL: "https://class-example-fac60.firebaseio.com",
+    projectId: "class-example-fac60",
+    storageBucket: "class-example-fac60.appspot.com",
+    messagingSenderId: "1002091060136"
   };
   firebase.initializeApp(config);
 
 // Variable to reference database
-  var database = firebase.data();
+  var database = firebase.database();
 
 // Initial values for the input fields
   var name = "";
@@ -28,34 +28,47 @@ $("#submit-btn").on("click", function(event){
     firstTime = $("#first-train-time").val().trim();
     frequency = $("#frequency").val().trim();
 
-    console.log(name);
+    console.log(name); 
     console.log(destination);
     console.log(firstTime);
     console.log(frequency);
 
-    // Code for handling the push
-    database.ref().push({
+    // Code for handling the set
+    database.ref().push().set({
       name: name,
       destination: destination,
       firstTime: firstTime,
       frequency: frequency
     });
 
+    
+  });
+  // Firebase watcher + initial loader for .on("value")
+  database.ref().on("value", function(snapshot) {
+
+    // Log everything that's coming out of snapshot
+    console.log(snapshot.val());
+    console.log(snapshot.val().name);
+    console.log(snapshot.val().destination);
+    console.log(snapshot.val().firstTime);
+    console.log(snapshot.val().frequency);
+
   });
 
-// Firebase watcher .on("child_added")
-database.ref().on("child_added", function(snapshot) {
-  var sv = snapshot.val();
+  // Firebase watcher .on("child_added")
+  database.ref().on("child_added", function(snapshot) {
+    // var sv = snapshot.val();
+    
+    // console.log(sv.name);
+    // console.log(sv.destination);
+    // console.log(sv.firstTime);
+    // console.log(sv.frequency);
+    
+    // Handle the errors
+  }, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code)
 
-  console.log(sv.name);
-  console.log(sv.destination);
-  console.log(sv.firstTime);
-  console.log(sv.frequency);
 
-  // Append the information taken from the user input to the train schedule table
-  $("#results-body").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrival + "</td><td>" + minsAway + "</td></tr>");
-
-  // Handle the errors
-}, function(errorObject) {
-  console.log("Errors handled: " + errorObject.code)
+    // Append the information taken from the user input to the train schedule table
+    $("#results-body").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrival + "</td><td>" + minsAway + "</td></tr>");
 });
